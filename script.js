4e8f6631283c2c1e8dfec53e67dc33c2a9bcd171
Ashish Rokade <ashishrokade944@gmail.com>
2026-09-14 16:32:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+function setEqualHeight() {
+    const columns = document.querySelectorAll(".column");
+
+    let maxHeight = 0;
+
+    columns.forEach(function (column) {
+        column.style.height = "auto";
+        maxHeight = Math.max(maxHeight, column.offsetHeight);
+    });
+
+    columns.forEach(function (column) {
+        column.style.height = maxHeight + "px";
+    });
+}
+
+window.addEventListener("load", setEqualHeight);
+window.addEventListener("resize", setEqualHeight);
